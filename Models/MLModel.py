55567fea807030b5da51dea59931eb943438de91
bird <bird@debian>
2017-12-17 05:51:18 -0500

    cluster

--- Models/MLModel.py
+++ Models/MLModel.py
@@ -1,11 +1,20 @@
 class MLModel:
     def __init__(self,data):
+        '''
+
+        :param data: data object, defined in ReadData as FetchingData
+        '''
         self.data=data
         self.name=''
         self.model=None
     def train(self):
         pass
     def predict(self,x):
+        '''
+
+        :param x: an array of input vectors
+        :return: an array of preidtcted labels
+        '''
         pass
     def save(self):
         pass
